7965dcd026813b591af044c2fe8621a54f54aee1
Ovidiu Gabor <ovidiu.gabor91@gmail.com>
2022-06-06 19:01:01 +0300

    Practice dynamic rendering and conditional content

--- ReactBasics/first-react-app/src/App.js
+++ ReactBasics/first-react-app/src/App.js
@@ -4,53 +4,54 @@ import ExpenseItemContainer from "./Components/Expenses/ExpenseItemContainer";
 import NewExpense from "./Components/NewExpense/NewExpense";
 import { useState } from "react";
 
-//function App() { 
-const App = () => { //Arrow function that have been implemented on ES6
-	
-	const expense = [
-		{
-			id: "e1",
-			title: "Toilet Paper",
-			amount: 94.12,
-			date: new Date(2020, 7, 14),
-		},
-		{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-		{
-			id: "e3",
-			title: "Car Insurance",
-			amount: 294.67,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			id: "e4",
-			title: "New Desk (Wooden)",
-			amount: 450,
-			date: new Date(2021, 5, 12),
-		},
-	];
+let DUMMY_DATA = [
+	{
+		id: "e1",
+		title: "Toilet Paper",
+		amount: 94.12,
+		date: new Date(2020, 7, 14),
+	},
+	{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+	{
+		id: "e3",
+		title: "Car Insurance",
+		amount: 294.67,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		id: "e4",
+		title: "New Desk (Wooden)",
+		amount: 450,
+		date: new Date(2021, 5, 12),
+	},
+];
+
+//function App() {
+const App = () => {
+	//Arrow function that have been implemented on ES6
+
 	//Alternativ to JSX
 	//Sort off the same as Document.createElement from javascript.
-	const[newExpense, setNewExpense] = useState(expense)
+	const [newExpense, setNewExpense] = useState(DUMMY_DATA);
 
 	const addExpenseHandler = (sentExpense) => {
-		expense.push(sentExpense)
-		console.log(expense)
-		setNewExpense(expense)
-	}
+		setNewExpense(prev => {
+			return [sentExpense, ...prev];
+		});
+	};
 
-/* 	return React.createElement(
+	/* 	return React.createElement(
 		"div",
 		{},
 		React.createElement(NewExpense, {onExpenseSend: {addExpenseHandler}}),
 		React.createElement(ExpenseItemContainer, {data: expense})
 	); */
-
-		return (
+	return (
 		<div>
-			<NewExpense  onExpenseSend={addExpenseHandler}/>
-			<ExpenseItemContainer data = {newExpense}></ExpenseItemContainer>
+			<NewExpense onExpenseSend={addExpenseHandler} />
+			<ExpenseItemContainer data={newExpense}></ExpenseItemContainer>
 		</div>
 	);
-}
+};
 
 export default App;
