6daa7d656c93e6745178533a09426b90f96bd1ea
Ovidiu Gabor <ovidiu.gabor91@gmail.com>
2022-07-20 22:41:17 +0300

    header

--- ReactAdvance/food-order-app/src/components/Layout/HeaderCardButton.js
+++ ReactAdvance/food-order-app/src/components/Layout/HeaderCardButton.js
@@ -1,15 +1,17 @@
 import CartIcon from "../Cart/CartIcon";
-
+import classes from "./HeaderCardButton.module.css"
 
 
 const HeaderCardButton = props => {
 
-    return <button>
-        <span>
+    return <button className={classes.button}>
+        <span className={classes.icon}>
         <CartIcon />
         </span>
-        <span></span>
-        <span></span>
+        <span>Your Card</span>
+        <span className={classes.badge}>
+            3
+        </span>
     </button>
 
 }
